f117b6b4aff7ea37457d62823f2584ad83b3c0c8
Vitaliy21 <aaron_21@ukr.net>
2019-07-10 12:50:35 +0300

    first node setup

--- web-app/web-app/src/index.js
+++ web-app/web-app/src/index.js
@@ -5,19 +5,16 @@ import './website.css';
 import Websites from './components/websites';
 import Create from './components/create';
 import Update from './components/update';
-//import serviceWorker from './serviceWorker';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
 	<Router>
 		<div>
-			<Route exact path='/' component={git} />
+			<Route exact path='/' component={Websites} />
 			<Route path='/create' component={Create} />
 			<Route path='/update/:id' component={Update} />
 		</div>
 	</Router>,
 document.getElementById('root'));
 
-//serviceWorker();
 serviceWorker.unregister();
-//test
